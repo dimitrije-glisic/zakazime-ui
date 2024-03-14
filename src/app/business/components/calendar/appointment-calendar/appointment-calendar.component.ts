61b670955cc28c084c206c40ee21367679ce2da9
Dimitrije Glisic <dimitrije.glisic.ext@riag-digital.com>
2024-03-14 10:23:29 +0100

    appointments management - add employee selection to calendar

--- src/app/business/components/calendar/appointment-calendar/appointment-calendar.component.ts
+++ src/app/business/components/calendar/appointment-calendar/appointment-calendar.component.ts
@@ -1,13 +1,16 @@
-import {ChangeDetectionStrategy, Component, OnInit, ViewEncapsulation} from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 import {CalendarEvent, CalendarView} from 'angular-calendar';
-import {addDays, addHours, startOfDay} from 'date-fns';
 import {EventColor} from 'calendar-utils';
 import {AppointmentService} from "../../../../public-layout/booking/services/appointment.service";
 import {Appointment} from "../../../../interfaces/appointment";
+import {BusinessService} from "../../../services/business-service";
+import {Business} from "../../../../interfaces/business";
+import {BusinessRichObject} from "../../../../interfaces/business-rich-object";
+import {Employee} from "../../../../interfaces/employee";
 
 @Component({
   selector: 'app-appointment-calendar',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  // changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './appointment-calendar.component.html',
   encapsulation: ViewEncapsulation.None,
   styleUrl: './appointment-calendar.component.css'
@@ -33,46 +36,61 @@ export class AppointmentCalendarComponent implements OnInit {
     secondary: '#FAE3E3'
   };
 
-  events: CalendarEvent[] = [
-    {
-      start: startOfDay(new Date()),
-      title: 'An event',
-      color: this.yellowColor
-    },
-    {
-      start: addHours(startOfDay(new Date()), 2),
-      end: new Date(),
-      title: 'Another event',
-      color: this.blueColor
-    },
-    {
-      start: addDays(addHours(startOfDay(new Date()), 2), 2),
-      end: addDays(new Date(), 2),
-      title: 'And another',
-      color: this.redColor
-    },
-  ];
+  events: CalendarEvent[] = [];
+  allAppointments: Appointment[] = [];
+  showingAppointments: Appointment[] = [];
+  selectedEmployee: Employee | undefined;
+  business: BusinessRichObject | undefined;
 
-  appointments: Appointment[] = [];
-
-  constructor(private appointmentService: AppointmentService) {
+  constructor(private businessService: BusinessService, private appointmentService: AppointmentService) {
   }
 
   ngOnInit(): void {
-    const businessId = 1;
     const date = new Date();
-    this.appointmentService.getAllAppointments(businessId, date).subscribe((appointments: any) => {
-      this.appointments = appointments;
-      this.events = this.appointments.map((appointment: Appointment) => {
-        return {
-          start: new Date(appointment.startTime),
-          end: new Date(appointment.endTime),
-          title: 'Appointment done by ' + appointment.employeeId,
-          color: appointment.employeeId % 2 === 0 ? this.yellowColor : this.blueColor
-        };
-      });
+    this.businessService.getBusinessCached().subscribe((business: Business | undefined) => {
+      if (business) {
+        this.loadData(business.id);
+      } else {
+        throw new Error('Business not found');
+      }
     });
   }
 
-}
+  private loadData(businessId: number) {
+    const date = new Date();
+    this.businessService.getBusinessRichObjectCached(businessId).subscribe((business: BusinessRichObject | undefined) => {
+      this.business = business;
+      this.selectedEmployee = business?.employees[0];
+      this.loadAppointments(businessId, date);
+    });
+  }
 
+  private loadAppointments(businessId: number, date: Date) {
+    this.appointmentService.getAllAppointments(businessId, date).subscribe((appointments: any) => {
+      this.allAppointments = appointments;
+      this.reloadEvents();
+      console.log(this.events);
+    });
+  }
+
+  private reloadEvents() {
+    this.showingAppointments = this.allAppointments.filter((appointment: Appointment) => {
+      return this.selectedEmployee?.id === appointment.employeeId;
+    });
+
+    this.events = this.showingAppointments.map((appointment: Appointment) => {
+      return {
+        start: new Date(appointment.startTime),
+        end: new Date(appointment.endTime),
+        title: 'Appointment done by ' + appointment.employeeId,
+        color: appointment.employeeId % 2 === 0 ? this.yellowColor : this.blueColor
+      };
+    });
+  }
+
+  onEmployeeClick(employee: Employee): void {
+    this.selectedEmployee = employee;
+    this.reloadEvents();
+  }
+
+}
--- src/app/interfaces/business-rich-object.ts
+++ src/app/interfaces/business-rich-object.ts
@@ -1,10 +1,12 @@
 import {UserDefinedCategory} from "./user-defined-category";
 import {Service} from "./service";
 import {Business} from "./business";
+import {Employee} from "./employee";
 
 export interface BusinessRichObject {
   business: Business;
   services: Service[];
   userDefinedCategories: UserDefinedCategory[];
+  employees: Employee[];
 }
 
