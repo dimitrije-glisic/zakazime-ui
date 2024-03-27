d4c1f00d37a9632f291ad55c2083cf4c3c092b6f
Dimitrije Glisic <dimitrije.glisic.ext@riag-digital.com>
2024-03-27 13:21:53 +0100

    business profile summary - improve content

--- src/app/public-layout/business-profile-summary/business-profile-summary.component.ts
+++ src/app/public-layout/business-profile-summary/business-profile-summary.component.ts
@@ -10,4 +10,33 @@ export class BusinessProfileSummaryComponent {
 
   @Input() business: Business | undefined;
 
+  getRating() {
+    return (Math.random() * 3 + 7).toFixed(1);
+  }
+
+  getRatingCount() {
+    return Math.floor(Math.random() * 100);
+  }
+
+  getLastAppointmentBookTime() {
+    const option1 = "pre 2 sata";
+    const option2 = "pre 1 sat";
+    const option3 = "pre 30 minuta";
+
+    //return random option
+    const options = [option1, option2, option3];
+    return options[Math.floor(Math.random() * options.length)];
+  }
+
+  getLastWorkingHour() {
+    return "20:00";
+  }
+
+  getFirstWorkingHourForTomorrow() {
+    return "08:00";
+  }
+
+  getLastWorkingHourForTomorrow() {
+    return "20:00";
+  }
 }
